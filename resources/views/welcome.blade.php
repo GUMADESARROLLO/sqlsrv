ecb8bfc7d955f0b94f01db427843739861a1d75a
Guma Master <analista.guma@gmail.com>
2022-07-24 08:38:28 -0600

    Ahora con ssh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Pero con SSH
                 </div>
 
                 <div class="links">
